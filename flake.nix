d2c8e729ead5faca8c2734e7f4054b65890bba69
Idris Raja <idris.raja@gmail.com>
2025-08-07 11:26:28 -0500

    update flakes

--- flake.nix
+++ flake.nix
@@ -2,11 +2,13 @@
   inputs.nixpkgs.url = "github:nixos/nixpkgs/24.11";
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.mksession = {
-    url = "github:idrisr/mksession";
+    # url = "github:idrisr/mksession";
+    url = "/home/hippoid/fun/mksession";
     inputs.nixpkgs.follows = "nixpkgs";
   };
   inputs.pdftc = {
-    url = "github:idrisr/pdftc";
+    # url = "github:idrisr/pdftc";
+    url = "/home/hippoid//fun/pdftc";
     inputs.nixpkgs.follows = "nixpkgs";
   };
 
