3ee499209abe4a5c638e7ee213b85927b72cc742
Idris Raja <idris.raja@gmail.com>
2025-08-07 11:20:18 -0500

    temp commit

--- flake.nix
+++ flake.nix
@@ -3,35 +3,26 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.mksession = {
     url = "github:idrisr/mksession";
+    inputs.nixpkgs.follows = "nixpkgs";
   };
   inputs.pdftc = {
     url = "github:idrisr/pdftc";
+    inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = inputs@{ nixpkgs, flake-utils, ... }:
-    let
-      system = flake-utils.lib.system.x86_64-linux;
-      pkgs = nixpkgs.legacyPackages.${system};
-      doc = pkgs.callPackage ./doc { };
-    in
     {
-      overlays = {
-        mksession = inputs.mksession.overlays.default;
-        pdftc = inputs.pdftc.overlays.default;
 
-        default = final: prev:
-          builtins.foldl'
+      overlays.default = final: prev:
+        let
+          merged = builtins.foldl'
             (acc: overlay: acc // overlay final prev)
             { }
             [
               inputs.mksession.overlays.default
               inputs.pdftc.overlays.default
             ];
-      };
-
-      packages.${system} = {
-        default = doc;
-        inherit doc;
-      };
+        in
+        merged;
     };
 }
